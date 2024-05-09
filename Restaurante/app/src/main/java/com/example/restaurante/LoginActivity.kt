cce773b7ea946404601ef188d8316532bcaa75ca
carlosgouraiserj <166538986+carlosgouraiserj@users.noreply.github.com>
2024-05-09 16:47:44 -0300

    LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -7,13 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.restaurante.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-// Cria a var binding que serve para manipular a tela.
+// Cria a variavel binding que serve para manipular a tela.
     private lateinit var binding:ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Define o layout de Login e a atribui a var binding.
+        // Define o layout de Login e a atribui a variavel binding.
         binding = ActivityLoginBinding.inflate(layoutInflater)
        // Manda Android usar o layout especificado pelo binding.root para exibir a interface do usuário da atividade atual
         setContentView(binding.root)
@@ -21,7 +21,7 @@ class LoginActivity : AppCompatActivity() {
         // Define um OnClicklistener para o botão "Entrar".
         binding.buttonEntrar.setOnClickListener {
 
-            // Define os valores da var para o nome de Usuário e a Senha inserida pelo usuário, converte para String e elimina os espaços em branco.
+            // Define os valores da variavel para o nome de Usuário e a Senha inserida pelo usuário, converte para String e elimina os espaços em branco.
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
@@ -29,14 +29,14 @@ class LoginActivity : AppCompatActivity() {
             if (username.equals("abc") && password.equals("123")) {
                 // Cria um Intent para iniciar o MainActivity
                 val i = Intent(this, MainActivity::class.java)
-                // Adiciona o nome de usuário como extra para a intent
+                // Adiciona o nome de usuário como extra para intent
                 i.putExtra("username", username)
               // Inicia o MainActivity
                 startActivity(i)
                // Finaliza o LoginActivity
                 finish()
             } else {
-                // Se o usuário e a senha estiverem errados, mostra uma mensagem de erro.
+                // Se o usuário e a senha estiverem incorretos, mostra uma mensagem de erro.
                 Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
             }
         }
